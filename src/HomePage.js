e25de58811868387f1ea1d1e4053cb963fd2f0f1
Yunfei <uryu.li3@gmail.com>
2018-01-14 21:20:17 -0500

    imported sass

---
+++ src/HomePage.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <img id="backgroundImage" src="images/backgroundPhoto.jpg" alt="" srcset=""/>
+      </div>
+    );
+  }
+}
+
+export default HomePage;
