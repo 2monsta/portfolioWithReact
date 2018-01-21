c9071a060544638a69461d9fb64750b408469444
Jason Li <Yunf.li37@gmail.com>
2018-01-20 22:20:12 -0500

    added projects

--- src/Projects.js
+++ src/Projects.js
@@ -1,23 +1,38 @@
 import React, { Component } from 'react';
 
+
+
+// could later look it up using the database
 class Projects extends Component {
 	render() {
 		return (
 			<div>
-				<div className='row'>
+				<div className='row videoContent'>
 					<div className='col-xs-12 col-sm-4'>
+						<span className='overlay'>
+							<span>HelloWorld</span>
+						</span>
 						<video src="video/mgntze.mp4"></video>
 					</div>
 					<div className='col-xs-12 col-sm-4'>
-						<video src="video/mgntze.mp4"></video>
+						<span className='overlay'>
+							<span>HelloWorld</span>
+						</span>
+						<video src="video/petBasket.mov"></video>
 					</div>
 					<div className='col-xs-12 col-sm-4'>
-						<video src="video/mgntze.mp4"></video>
+						<span className='overlay'>
+							<span>HelloWorld</span>
+						</span>
+						<video src="video/NavApp.mov"></video>
 					</div>
 				</div>
 				<div className='row'>
 					<div className='col-xs-12 col-sm-4'>
-						<span>Magnitize</span>
+						<span className='overlay'>
+							<span>HelloWorld</span>
+						</span>
+						<video src="video/ToDo.mov"></video>
 					</div>
 				</div>
 			</div>
